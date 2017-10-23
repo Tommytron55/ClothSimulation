e112d0848ef4fa6296eaedb93b6fb694bfe761b9
Thomas Male <tommytron2@yahoo.com.au>
2017-10-23 17:31:30 +1300

    Added multi threading though framerate is still pretty trash, might just have to simply reduce the amount of verticies in the scene

---
+++ ClothSimulation/Constraint.cpp
@@ -0,0 +1,22 @@
+#include "Constraint.h"
+
+Constraint::Constraint()
+{
+}
+
+
+Constraint::~Constraint()
+{
+}
+
+
+
+void Constraint::satisfyConstraint()
+{
+	glm::vec3 p1_to_p2 = p2->getPos() - p1->getPos(); // vector from p1 to p2
+	float current_distance = glm::length(p1_to_p2); // current distance between p1 and p2
+	glm::vec3 correctionVector = Util.ScaleVec3(p1_to_p2, (1 - rest_distance / current_distance)); // The offset vector that could moves p1 into a distance of rest_distance to p2
+	glm::vec3 correctionVectorHalf = Util.ScaleVec3(correctionVector, 0.5); // Lets make it half that length, so that we can move BOTH p1 and p2.
+	p1->offsetPos(correctionVectorHalf); // correctionVectorHalf is pointing from p1 to p2, so the length should move p1 half the length needed to satisfy the constraint.
+	p2->offsetPos(-correctionVectorHalf); // we must move p2 the negative direction of correctionVectorHalf since it points from p2 to p1, and not p1 to p2.	
+}
---
+++ ClothSimulation/Particle.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "Utils.h"
+
+class Particle
+{
+private:
+	bool movable; // can the particle move or not ? used to pin parts of the cloth
+
+	float mass; // the mass of the particle (is always 1 in this example)
+	glm::vec3 pos; // the current position of the particle in 3D space
+	glm::vec3 old_pos; // the position of the particle in the previous time step, used as part of the verlet numerical integration scheme
+	glm::vec3 acceleration; // a vector representing the current acceleration of the particle
+	glm::vec3 accumulated_normal; // an accumulated normal (i.e. non normalized), used for OpenGL soft shading
+
+public:
+	Particle(glm::vec3 pos) : pos(pos), old_pos(pos), acceleration(glm::vec3(0, 0, 0)), mass(1), movable(true), accumulated_normal(glm::vec3(0, 0, 0)) {}
+	Particle();
+	~Particle();
+
+	void addForce(glm::vec3 f);
+
+	/* The method is called by Cloth.time_step()
+	Given the equation "force = mass * acceleration" the next position is found through verlet integration*/
+	void timeStep(float _Damping, GLfloat _DeltaTime);
+
+	glm::vec3& getPos();
+
+	void resetAcceleration();
+
+	void offsetPos(const glm::vec3 v);
+
+	void makeUnmovable();
+
+	void addToNormal(glm::vec3 normal);
+
+	glm::vec3& getNormal();
+
+	void resetNormal();
+
+};
